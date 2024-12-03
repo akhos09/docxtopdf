26d2bb3d620c66b6e76f2e6d5d0741922575f089
Ak740s <pablofdezlpz09@gmail.com>
2024-12-03 20:35:15 +0100

    functions for selecting folders and some testing

--- docxtopdf.py
+++ docxtopdf.py
@@ -1,4 +1,9 @@
 import subprocess
+import tkinter
+from tkinter import filedialog as fd
+from tkinter import Tk
+from spire.doc import *
+from spire.doc.common import *
 def check():
     try:
         import spire.doc
@@ -21,6 +26,28 @@ def check():
             print('Installation completed. Execute the script again with Spire.Doc installed.')
         else:
             print('Exiting...')
-def convert():
-    print('hola')
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+def select_folder():
+    print('Please select the directory where the .docx files are located: ')
+    root = tkinter.Tk()
+    root.withdraw()
+    root.wm_attributes('-topmost', 1)
+    folder_selected = fd.askdirectory()
+    root.destroy()
+    return folder_selected
+
+def select_file():
+    filetypes = ('docx files', '.docx')
+    print('Please select the .docx file do you want to convert: ')
+    root = tkinter.Tk()
+    root.withdraw()
+    root.wm_attributes('-topmost', 1)
+    file_path = fd.askopenfilename()
+    root.destroy()
+    return file_path
+    
+def convert_dir():
+    folder_selected = select_folder()
+    for i in select_folder(folder_selected):    
+        print(f'i')
 check()
